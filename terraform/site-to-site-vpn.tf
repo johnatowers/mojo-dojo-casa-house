625f2b5890b34a1ac316e7a44fee62b9ec8fe066
Alex Towers <johnatowers@gmail.com>
2023-08-11 17:34:19 -0400

    added vpc to tf

--- terraform/site-to-site-vpn.tf
+++ terraform/site-to-site-vpn.tf
@@ -1,6 +1,14 @@
 # __generated__ by Terraform
 # Please review these resources and move them into your main configuration files.
 
+# __generated__ by Terraform
+resource "aws_vpc" "myvpc" {
+  cidr_block                           = "172.31.0.0/16"
+  enable_dns_hostnames                 = true
+  enable_dns_support                   = true
+  instance_tenancy                     = "default"
+}
+
 # __generated__ by Terraform from "cgw-08786c525594ec8ab"
 resource "aws_customer_gateway" "cgw" {
   bgp_asn         = "65000"
